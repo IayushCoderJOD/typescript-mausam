0321c3a8709a7da20ab90299f32850f6bc37b480
ayush tyagi <ayushtyagi4810poco@gmail.com>
2024-01-02 16:17:43 +0530

    initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-6xl'>
+      Going to built this mausam app in next few days in typescript
+    </div>
+  );
+}
+
+export default App;
